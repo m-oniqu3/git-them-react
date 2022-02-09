39842f40a3ceca9c95dc996a2bfb143afc4bfcfc
Monique McIntyre <skaiwilliams85@gmail.com>
2022-02-08 23:39:42 -0500

    styled the profile details and added the info

--- src/components/UserDetails/ProfileDetails.js
+++ src/components/UserDetails/ProfileDetails.js
@@ -1,5 +1,6 @@
 import React from "react";
-import styled from "./ProfileDetails.module.css";
+//import styled from "./ProfileDetails.module.css";
+import global from "./ProfileGlobalStyles.module.css";
 
 function ProfileDetails({
   name,
@@ -10,8 +11,25 @@ function ProfileDetails({
   followers,
   followersUrl,
   gists,
+  gistsUrl,
 }) {
-  return <div className={styled.container}></div>;
+  return (
+    <section className={global.wrapper}>
+      <h1>{name}</h1>
+      <p className={global.item}>Bio: {bio}</p>
+      <p className={global.item}>Location: {location}</p>
+      <a className={global.item} href={gistsUrl}>
+        Public Gists: {gists}
+      </a>
+      <a className={global.item} href={followingUrl}>
+        Following: {following}
+      </a>
+
+      <a className={global.item} href={followersUrl}>
+        Followers: {followers}
+      </a>
+    </section>
+  );
 }
 
 export default ProfileDetails;
